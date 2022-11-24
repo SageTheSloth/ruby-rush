e04d8b98596ed70b8b0c5180294c0f556e4b62f1
SageTheSloth <soft.sand5107@fastmail.com>
2022-11-23 20:51:25 -0600

    Initial project setup.

---
+++ src/main/java/net/sagethesloth/rubyrush/RubyRush.java
@@ -0,0 +1,16 @@
+package net.sagethesloth.rubyrush;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RubyRush implements ModInitializer {
+
+	public static final String MOD_ID = "rubyrush";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/sagethesloth/rubyrush/RubyRushClient.java
@@ -0,0 +1,10 @@
+package net.sagethesloth.rubyrush;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubyRushClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
